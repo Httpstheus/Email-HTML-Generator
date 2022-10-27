631c7ca8806dd7359ac530b53b3ffcc5b5145733
Matheus Rodrigues <84551135+Httpstheus@users.noreply.github.com>
2022-10-27 08:41:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -62,8 +62,11 @@ this.$(".btnCustom").click(function() {
           switch(tipo) {
             // Message for Option == Mundial Editora
             case 'MundialEditora':
-                document.documentElement.style.setProperty('--primary-color', '#004f9e');
+                // document.documentElement.style.setProperty('--primary-color', '#004f9e');
+                document.getElementById('name-welcome').innerHTML = "<h2 style=\"color: #004f9e;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Fugiat illum asperiores ducimus adipisci totam amet</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>A nostrum totam ipsum voluptatem quod officiis perspiciatis <br> ipsam, reprehenderit <br>iusto nobis consequuntur non nesciunt doloremque <br>commodi cumque id fugit delectus provident? </b>  </p>"
                 document.getElementById('how-agend').innerHTML = "<img src=\"http://marketing.mixd.com.br/admin/temp/user/128/mundial_banner_pagamento.jpg\" width=\"500px\">";
+                document.getElementById('contatos').style.backgroundColor ="#004f9e"
+                document.getElementById('footer-contatos').style.backgroundColor ="#004f9e"
 
             // footer
               document.getElementById('footer-contatos').innerHTML = "<h2>Central de Atendimento</h2><p>0800 779 4000<br/>negocie@mundialeditora.com.br</p>"
@@ -71,8 +74,11 @@ this.$(".btnCustom").click(function() {
                 break;
             // Message for Option == Bookplay
             case 'Bookplay':
-                document.documentElement.style.setProperty('--primary-color', '#48a0dc');
+                // document.documentElement.style.setProperty('--primary-color', '#48a0dc');
+                document.getElementById('name-welcome').innerHTML = "<h2 style=\"color: #48a0dc;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Fugiat illum asperiores ducimus adipisci totam amet</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>A nostrum totam ipsum voluptatem quod officiis perspiciatis <br> ipsam, reprehenderit <br>iusto nobis consequuntur non nesciunt doloremque <br>commodi cumque id fugit delectus provident? </b>  </p>"
                 document.getElementById('how-agend').innerHTML = "<img src=\"http://marketing.mixd.com.br/admin/temp/user/128/bookplay_banner_pagamento.jpg\" width=\"500px\">";
+                document.getElementById('contatos').style.backgroundColor ="#48a0dc"
+                document.getElementById('footer-contatos').style.backgroundColor ="#48a0dc"
             
             // footer
             document.getElementById('footer-contatos').innerHTML = "<h2>Central de Atendimento</h2><p>0800 779 6000<br/>negocie@bookplay.com.br</p>"
@@ -80,8 +86,11 @@ this.$(".btnCustom").click(function() {
                 break;
             // Message for Option == EditBrasiç
             case 'EditBrasil':
-                document.documentElement.style.setProperty('--primary-color', '#12a19b');
+                // document.documentElement.style.setProperty('--primary-color', '#12a19b');
+                document.getElementById('name-welcome').innerHTML = "<h2 style=\"color: #12a19b;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Fugiat illum asperiores ducimus adipisci totam amet</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>A nostrum totam ipsum voluptatem quod officiis perspiciatis <br> ipsam, reprehenderit <br>iusto nobis consequuntur non nesciunt doloremque <br>commodi cumque id fugit delectus provident? </b>  </p>"
                 document.getElementById('how-agend').innerHTML = "<img src=\"http://marketing.mixd.com.br/admin/temp/user/128/editbrasil_banner_pagamento.jpg\" width=\"500px\">";
+                document.getElementById('contatos').style.backgroundColor ="#12a19b"
+                document.getElementById('footer-contatos').style.backgroundColor ="#12a19b"
                 
             // footer
               document.getElementById('footer-contatos').innerHTML = "<h2>Central de Atendimento</h2><p>0800 779 4000<br/>negocie@mundialeditora.com.br</p>"
@@ -90,16 +99,22 @@ this.$(".btnCustom").click(function() {
 
             // Message for Option == Faculdade Play
             case 'FaculdadePlay':
-                document.documentElement.style.setProperty('--primary-color', '#004f9e');
+                // document.documentElement.style.setProperty('--primary-color', '#004f9e');
+                document.getElementById('name-welcome').innerHTML = "<h2 style=\"color: #004f9e;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Fugiat illum asperiores ducimus adipisci totam amet</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>A nostrum totam ipsum voluptatem quod officiis perspiciatis <br> ipsam, reprehenderit <br>iusto nobis consequuntur non nesciunt doloremque <br>commodi cumque id fugit delectus provident? </b>  </p>"
                 document.getElementById('how-agend').innerHTML = "<img src=\"http://marketing.mixd.com.br/admin/temp/user/128/faculdadeplay_banner_pagamento.jpg\" width=\"500px\">";
+                document.getElementById('contatos').style.backgroundColor ="#004f9e"
+                document.getElementById('footer-contatos').style.backgroundColor ="#004f9e"
                 
             // footer
             document.getElementById('footer-contatos').innerHTML = "<h2>Central de Atendimento</h2><p>0800 777 2020<br/>pagamento@faculdadeplay.edu.br</p>"
                 break;
             // Message for Option == Faculdade Bookplay
             case 'FaculdadeBookplay':
-                document.documentElement.style.setProperty('--primary-color', '#004f9e');
+                // document.documentElement.style.setProperty('--primary-color', '#004f9e');
+                document.getElementById('name-welcome').innerHTML = "<h2 style=\"color: #004f9e;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Fugiat illum asperiores ducimus adipisci totam amet</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>A nostrum totam ipsum voluptatem quod officiis perspiciatis <br> ipsam, reprehenderit <br>iusto nobis consequuntur non nesciunt doloremque <br>commodi cumque id fugit delectus provident? </b>  </p>"
                 document.getElementById('how-agend').innerHTML = "<img src=\"http://marketing.mixd.com.br/admin/temp/user/128/faculdadebookplay_banner_pagamento.jpg\" width=\"500px\">";
+                document.getElementById('contatos').style.backgroundColor ="#004f9e"
+                document.getElementById('footer-contatos').style.backgroundColor ="#004f9e"
                 
             // footer
             document.getElementById('footer-contatos').innerHTML = "<h2>Central de Atendimento</h2><p>0800 777 2020<br/>sac@faculdadebookplay.edu.br</p>"
@@ -109,3 +124,4 @@ this.$(".btnCustom").click(function() {
 
         }
       }) 
+    
